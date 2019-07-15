9b592df7b7fd3906c9b7caa33ad52d3bf7cf253e
felton <wudadongfen@126.com>
2019-07-15 14:13:21 +0800

    添加搜索

--- src/main/java/com/how2java/tmall/config/WebMvcConfigurer.java
+++ src/main/java/com/how2java/tmall/config/WebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.how2java.tmall.config;
 
 import com.how2java.tmall.interceptor.LoginInterceptor;
+import com.how2java.tmall.interceptor.OtherInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,8 +14,14 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         return new LoginInterceptor();
     }
 
+    @Bean
+    public OtherInterceptor getOtherIntercepter() {
+        return new OtherInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getOtherIntercepter()).addPathPatterns("/**");
         registry.addInterceptor(getLoginIntercepter()).addPathPatterns("/**");
     }
 }
